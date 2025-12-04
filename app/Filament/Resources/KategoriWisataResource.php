95af43a0bfd38692227b2d0dc909f8b703970d59
Muhammad Irvansyah <mirvansyah132@gmail.com>
2025-12-04 07:46:58 +0700

    resource

--- app/Filament/Resources/KategoriWisataResource.php
+++ app/Filament/Resources/KategoriWisataResource.php
@@ -5,43 +5,36 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\KategoriWisataResource\Pages;
 use App\Models\KategoriWisata;
 use Filament\Forms;
-use Filament\Resources\Resource;
 use Filament\Forms\Form;
+use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 
 class KategoriWisataResource extends Resource
 {
     protected static ?string $model = KategoriWisata::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-tag';
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\TextInput::make('nama_kategori')
-                    ->label('Nama Kategori')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+        return $form->schema([
+            Forms\Components\TextInput::make('nama_kategori')
+                ->required()
+                ->maxLength(255),
+        ]);
     }
 
     public static function table(Table $table): Table
     {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('nama_kategori')->sortable()->searchable(),
-                Tables\Columns\TextColumn::make('created_at')->dateTime()->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')->dateTime()->toggleable(isToggledHiddenByDefault: true),
-            ])
+        return $table->columns([
+            Tables\Columns\TextColumn::make('nama_kategori')->searchable()->sortable(),
+            Tables\Columns\TextColumn::make('created_at')->dateTime()->sortable(),
+        ])
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
 
--- app/Filament/Resources/KategoriWisataResource/Pages/CreateKategoriWisata.php
+++ app/Filament/Resources/KategoriWisataResource/Pages/CreateKategoriWisata.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\KategoriWisataResource\Pages;
 
 use App\Filament\Resources\KategoriWisataResource;
+use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateKategoriWisata extends CreateRecord
